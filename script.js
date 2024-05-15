ebee1057a22d71a80c47d718d247d3c7019d82f8
Modey Codey <phawngam@hotmail.com>
2024-05-15 15:00:59 +0700

    Ex2

---
+++ script.js
@@ -0,0 +1,12 @@
+// ex2 p1
+const people = ["Sofia", "David", "Juan"];
+
+// ex2 p2
+people.push("Sara", "Augustin");
+people.shift("Sofia");
+console.log(people);
+
+// ex2 p3
+people.splice(1, 0, "Renata");
+people.push("Elena");
+console.log(people);
